85f2c40b6ac7a13f2f142f4c73a67c06ad98672b
winst <kochura.kirr1@gmail.com>
2025-12-04 17:17:11 +0300

    solutions.js

---
+++ solutions.js
@@ -0,0 +1,42 @@
+// Задача 1
+const concatenate = function(value1, value2) {
+return value1.replace(/\s/g, '') + value2.replace(/\s/g, '');
+}
+
+//Задача 2
+const stringLength = function(value) {
+return value.length;
+}
+
+//Задача 3
+const stringTemplate = function(firstName, lastName) {
+return 'Hello, ' + firstName + ' ' + lastName + '!';
+}
+
+//Задача 4
+const getChar = function(value, index) {
+return value[index - 1];
+}
+
+// Задача 5
+const removeOccurrence = function(str, value) {
+const index = str.indexOf(value);
+if (index === -1) {
+return str;
+}
+return str.slice(0, index) + str.slice(index + value.length);
+}
+
+//Задача 6
+const getRectangle = function(width, height) {
+ if (width < 2 || height < 2) {
+  return '*'.repeat(width) + (height > 1 ? '\n' + '*'.repeat(width) : '');
+ }
+   let result = '';
+    result += '*'.repeat(width) + '\n';
+    for (let i = 0; i < height - 2; i++) {
+    result += '*' + ' '.repeat(width - 2) + '*\n';
+    }
+result += '*'.repeat(width);
+    return result;
+};
